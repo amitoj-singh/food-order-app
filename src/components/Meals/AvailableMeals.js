457fe127096b9e7a507042cffa8b36284b5f58c1
Amitoj Singh <amitoj.98@gmail.com>
2022-02-21 13:24:25 +0530

    Error Handling added

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -7,10 +7,16 @@ import MealItem from "./MealItem/MealItem";
 const AvailableMeals = () => {
     const [meals, setMeals] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [httpError, setHttpError] = useState();
 
     useEffect(() => {
         const fetchMeals = async () => {
             const response = await fetch('https://react-http-2a27e-default-rtdb.firebaseio.com/meals.json');
+
+            if (!response.ok) {
+                throw new Error('Something went wrong!');
+            }
+
             const responseData = await response.json();
 
             let loadedMeals = [];
@@ -25,13 +31,24 @@ const AvailableMeals = () => {
             setIsLoading(false);
         };
 
-        fetchMeals();
+        fetchMeals().catch(error => {
+            setIsLoading(false);
+            setHttpError(error.message);
+        });
     }, []);
     
     if (isLoading) {
         return (<section className={classes.mealsLoading}>
             <p>Loading...</p>
         </section>)
+    }
+
+    if (httpError) {
+        return (
+            <section className={classes.mealsError}>
+                <p>{httpError}</p>
+            </section>
+        )
     }
 
     const mealsList = meals.map(meal => 
