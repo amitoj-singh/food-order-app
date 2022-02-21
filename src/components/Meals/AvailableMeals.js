ad465d31aa49d49fc012d77fba2fb0ba78aa7a5b
Amitoj Singh <amitoj.98@gmail.com>
2022-02-21 13:12:43 +0530

    fetching data from firebase backend

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,37 +1,40 @@
+import { useEffect, useState } from "react";
+
 import Card from "../UI/Card";
 import classes from "./AvailableMeals.module.css";
 import MealItem from "./MealItem/MealItem";
-
-const DUMMY_MEALS = [
-    {
-        id: 'm1',
-        name: 'Sushi',
-        description: 'Finest fish and veggies',
-        price: 22.99,
-    },
-    {
-        id: 'm2',
-        name: 'Schnitzel',
-        description: 'A german specialty!',
-        price: 16.5,
-    },
-    {
-        id: 'm3',
-        name: 'Barbecue Burger',
-        description: 'American, raw, meaty',
-        price: 12.99,
-    },
-    {
-        id: 'm4',
-        name: 'Green Bowl',
-        description: 'Healthy...and green...',
-        price: 18.99,
-    },
-];
   
-const availableMeals = () => {
+const AvailableMeals = () => {
+    const [meals, setMeals] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchMeals = async () => {
+            const response = await fetch('https://react-http-2a27e-default-rtdb.firebaseio.com/meals.json');
+            const responseData = await response.json();
+
+            let loadedMeals = [];
+
+            for (const key in responseData) {
+                loadedMeals.push({
+                    id: key,
+                    ...responseData[key]
+                });
+            }
+            setMeals(loadedMeals);
+            setIsLoading(false);
+        };
+
+        fetchMeals();
+    }, []);
     
-    const mealsList = DUMMY_MEALS.map(meal => 
+    if (isLoading) {
+        return (<section className={classes.mealsLoading}>
+            <p>Loading...</p>
+        </section>)
+    }
+
+    const mealsList = meals.map(meal => 
         <MealItem
             id={meal.id}
             key={meal.id}
@@ -50,4 +53,4 @@ const availableMeals = () => {
     </section>
 };
 
-export default availableMeals;
+export default AvailableMeals;
